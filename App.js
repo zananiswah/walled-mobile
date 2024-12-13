d101f781760cd650c5fa826601f87503aad251b2
Zana <zana.niswah@gmail.com>
2024-12-13 10:30:14 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Image source={require('./assets/logo.png')} style={styles.logo} />
+      
+      <TextInput 
+        style={styles.input} 
+        placeholder="Email" 
+        placeholderTextColor="#aaa" 
+        keyboardType='email-address'
+      />
+      
+      <TextInput 
+        style={styles.input} 
+        placeholder="Password" 
+        placeholderTextColor="#aaa" 
+        secureTextEntry={true} 
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" hidden />
     </View>
   );
 }
@@ -16,5 +36,41 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  logo: {
+    // width: 100,
+    // height: 100,
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    backgroundColor: '#f9f9f9',
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#4DB6AC',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 15,
+    width: '100%',
+    alignItems: 'center',
+
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
